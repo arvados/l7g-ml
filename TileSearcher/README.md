2f164bdfae81132d37554d4fcca5164838c76996
Kevin Fang <kfang@curii.com>
2019-01-14 19:26:58 +0000

    closes #14731 Improved documentation, added tile/rsid searching as a function, added keep references
    Arvados-DCO-1.1-Signed-off-by: Kevin Fang <kfang@curii.com>

--- TileSearcher/README.md
+++ TileSearcher/README.md
@@ -1,39 +1,118 @@
-# Tile Searcher
-### Kevin Fang, 2017-19
-Python scripts that allow for information to be extracted from tiles.
+# Tile Variant Searcher & RSID Lookup
+### Kevin Fang, Curoverse Research, 2017-19
+Python scripts that extract information from tiles.
 
-Contains a script (`getTileVariants.py`) that retrieves tile information, including step, path, phase, and variants from assembly files that and can also perform Clustal Omega alignments on the variants.
+Contains a script (`getTileVariants.py`) that retrieves tile step, path, phase, and variants from assembly files that and can also perform Clustal Omega alignments on the variants.
 
 Contains another script (`getRsids.py`) to retrieve RSIDs for specific variants. 
 
-A Dockerfile is also provided for easy dependency installation. To call it, run `./runInDocker.sh "<command line arguments here>"`. For example, `./runInDocker.sh "./getTileVariants -i 1234"`. If the docker build is not named kfang/tile-tools, then change the docker image name in the shell script.
+#### Setup instructions to get running in Docker  
+1. Make sure Docker is installed on your machine  
+2. Clone and open this repository in the command line (`git clone https://github.com/kevin-fang/tile-searcher && cd tile-searcher`)  
+3. Run `docker pull kfangcurii/tile-tools`  
+4. Set up your Arvados API Tokens (skip this if you're running on an Arvados shell node)
+5. Run `./setup.sh` to mount keep
+6. Search for tiles using the following command: `./runInDocker.sh "<command line arguments here>"`, using command line arguments normally. For example, `./runInDocker.sh "./getTileVariants.py -i 1234 56789"` or `./runInDocker.sh "./getRsids.py -i 1234 -v 1"`.
 
-To get up and running, set the Arvados API keys and download your assembly files. The included `./setup.sh` file downloads GRCh37 assembly files and high quality naming info for the Harvard PGP. Then, modify `config.yml` to reflect where the data is located. For `getTileVariants.py`, there are a list of command line arguments you can use. Alternatively, you can change the file `config.yml` to specify features that the searcher should use and point to the necessary data files. A sample configuration file is provided. Note that `getRsids.py` _requires_ that the all the files requirements are specified in `config.yml`.
-
+#### Notes:
+- If the docker build is not named `kfangcurii/tile-tools`, then change the docker image name in the shell script. Alternatively, build it yourself from the Dockerfile included in the `docker/` directory and change the docker name in the shell script
+- Modify `config.yml` to reflect where the data is located. A sample configuration file is provided that represents the full tile set. 
+- For `getTileVariants.py`, you can choose which config arguments to include depending on search requirements. Instructions are included in `config.yml` file.
+- However, `getRsids.py` uses every feature of `getTileVariants.py`, so it _requires_ that the all the files requirements are specified in `config.yml`.
 
 ## Tile Variant Searching
 
 The script `getTileVariants.py` is used to retrieve tile information, including step, path, phase, and variants from assembly files. Also contains a multiple sequence aligner to compare with all the variants (using Clustal Omega alignment).
 
-Note that to run the ClustalW alignment at the end to check for variant differences, you must have the [Clustal Omega](http://www.clustal.org/omega/) package installed and have `clustalo` added to your path.
+Note that to run the ClustalO alignment at the end to check for variant differences, you must have the [Clustal Omega](http://www.clustal.org/omega/) package installed and have `clustalo` added to your path.
 
-To write the tile data to an output folder, include the argument `--write-to-file` when calling `getTileVariants.py` and it will create a folder with output.
-
-A sample call for tile variants 1234 and 67890 can look something like `./getTileVariants.py -i 1234 67890 --tile-info ./tiling-files/hiq-pgp-info --location --assembly-fwi='./tiling-files/assembly.00.hg19.fw.fwi' --diff-indices --keep=./keep/by_id/su92l-4zz18-fkbdz2w6b25ayj3 --variant-diffs --base-pair-locations --assembly-gz='./tiling-files/assembly.00.hg19.fw.gz'`. 
-
-Alternatively, if `config.yml` set, just call `python getTileVariants.py -i 1234 67890`.
-
-List of commandline arguments for `getTileVariants.py`:  
-- `-i` or `--index` lets you provide a list of indices for the tile searcher to run on.  
-- `--tile-info` points to the location of the tile names. For the Harvard Personal Genome Project, it is called `hiq-pgp-info`.  
-- `--location` is included if the script should output the tile path, step, and phase.  
-- `--variant-diffs` is included if the script should output the ClustalW alignment, showing the differences between the tile variants.  
-- `--base-pair-locations` is included if the script should output base pair locations of the tile.  
-- `--diff-indices` is included if the script should output the indices where variants are different.  
-- `--write-to-file` is included if the script should write the output to a file instead. By default, it writes to a folder called `output`.
+Deprecated: ability to use command line arguments (too cluttered). Instead, call `python getTileVariants.py -i 1234 67890` after setting `config.yml`.
 
 ## RSID Searching
 
 The script `getRsids.py` is used to retrieve a list of possible RSIDs for SNPs/INDELS located in a variant of a genome. It compares a variant with the most common tile, and uses Mutalyzer 2 to locate the variations. 
 
-A sample call to check for possible RSIDs on the first variant of tile 1234 would look like `./getRsids.py -i 1234 -v 1`. Note that to call `getRsids.py`, `config.yml` _must_ be set.
+A sample call to check for possible RSIDs on the first variant of tile 1234 would look like `./getRsids.py -i 1234 -v 1`.
+
+## Tile Searching as a function
+
+If you would like to use the output of the tile searching script in Python, you do not need to manually parse the output. `getTileVariants.py` is available as a function, callable from python. See the example below:  
+```python
+# import modules for tile variant searching
+import getTileVariants, application
+
+# search for index 1234, while suppressing processing output
+tile_info = getTileVariants.tile_iteration(1234, suppress_output=True)
+
+info_dict = tile_info.to_dict()
+
+'''
+info_dict looks like this:
+{
+    "chrom": "9",
+    "path": "01c4",
+    "position_end": "136140011",
+    "position_start": "136139786",
+    "step": "0369"
+}
+'''
+```
+
+There is more tile information available in the `tile_info` object that is returned. See below:   
+```python
+class Tile:
+    def __init__(self, index):
+        ...
+
+        # tile path, step, and phase 
+        self.path = None
+        self.phase = None
+        self.step = None
+
+        # string representation of tile
+        self.tile_str = None
+
+        # tile variants
+        self.variants = None
+
+        # raw output from clustalo
+        self.clustalo_output = None
+
+        # calculated diffs from clustalo
+        self.clustalo_diffs = None
+        
+        # raw text output from base pair location searching
+        self.bp_output = None       
+
+        # mapping from tile indexes to chromosomal locations
+        self.diffs_map = None
+```
+
+## RSID Searching as a function
+
+To use the RSID Searching from inside python, simply import it as a module. 
+
+```python
+# import module
+import getRsids
+# search for RSIDs for tile with index 1234, 1st variant.
+# returns a dictionary with tile (variant) sequence, common sequence, variant, mutations, and index
+info_dict = getRsids.rsid_search(1234, 1)
+
+'''
+info_dict looks like this:
+{  
+   "tile_sequence":"0000.00.0269.001+1,478d344b887e4dd7ed5d4af3c9cd8a76,atgccgctgcggggcacgttggtcctttccgcactcggggtccccggcggcctcacgcgtccgtgcagcggaggcttcctgagccccctggagagcctggcctgggcccgggtgtggagaccctcccgggctttcaatccgggcaggaggcagatggcagacaaaaaaaaaacataagagaaccgaattaggtgggtggcctgggtggacaaaagccttcttgacgccgggtggtcccaaaggcttctg",
+   "variant":1,
+   "mutations":{  
+      "936633_936644delinsaaaaaaaaaaca":[  
+         ("rs1017325702", "936636", "G", "C"),
+         ("rs887210492",  "936643", "C", "A")
+      ]
+   },
+   "common_sequence":"0000.00.0269.000+1,86945238e5aa7ad4ff4b407a3c442c46,atgccgctgcggggcacgttggtcctttccgcactcggggtccccggcggcctcacgcgtccgtgcagcggaggcttcctgagccccctggagagcctggcctgggcccgggtgtggagaccctcccgggctttcaatccgggcaggaggcagatggcagactcagcagtcacgtaagagaaccgaattaggtgggtggcctgggtggacaaaagccttcttgacgccgggtggtcccaaaggcttctg",
+   "index":1234
+}
+'''
+```
+
--- TileSearcher/getRsids.py
+++ TileSearcher/getRsids.py
@@ -1,31 +1,43 @@
 #!/usr/bin/env python
-import subprocess, argparse
+import getTileVariants
+import application
+import subprocess
+import argparse
 from extractor import describe_dna
 import yaml
 import sys
 
-# set up argument parsing
-parser = argparse.ArgumentParser(description="Retrieve a list of possible RSIDs a tile variant may contain")
+CONFIG_FILENAME = "config.yml"
 
-parser.add_argument("-i", "--index", type=int, help="Index of tile to search", required=True)
-parser.add_argument("-v", "--variant", type=int, help="Which variant to search for RSIDs for (e.g. for varval 1, it will use the variant with id ending in .001)")
 
-args = parser.parse_args()
-
-# import afterwards, because the process of importing takes a long time and arguments should be parsed beforehand
-import application, getTileVariants
+# load reference vcf from config
+with open(CONFIG_FILENAME, 'r') as stream:
+    data_args = yaml.load(stream)
+    ref_vcf = data_args['ref_vcf']
+    
+# import afterwards, because the process of importing takes a long time
+# and arguments should be parsed beforehand
 
 # mutation class, created for simpler use
+
+
 class Mutation:
-    def __init__(self, start,change, mutation_type, chrom, stop=-1, pre_offsetted=False):
+    def __init__(
+            self,
+            start,
+            change,
+            mutation_type,
+            chrom,
+            stop=-1,
+            pre_offsetted=False):
         self.start = int(start)
         self.stop = int(stop)
         self.change = change
         self.mutation_type = mutation_type
         self.offsetted = pre_offsetted
         self.chrom = chrom
-       
-    # adds a specified offset to start and stop 
+
+    # adds a specified offset to start and stop
     def offset(self, offset_val):
         if not self.offsetted:
             self.start = self.start + int(offset_val) - 24
@@ -36,71 +48,43 @@ class Mutation:
             self.offsetted = True
         else:
             print("Already offsetted")
-           
-    # queries for the possible rsids of the mutation using bcftoolsj 
+
+    # queries for the possible rsids of the mutation using bcftoolsj
     def rsid_query(self):
         if self.offsetted:
             query_str = "{}:{}-{}".format(self.chrom, self.start, self.stop)
             #bcfstr = 'bcftools query -f' + '\'chr%CHROM %ID %POS %REF>%ALT\\n\'' + ' -r ' + query_str + ' ' + ref_vcf
             #resultquery = subprocess.check_output(bcfstr,shell=True)
-            proc = subprocess.Popen(["bcftools", "query", "-f", r"%CHROM %ID %POS %REF %ALT\n", "-r", query_str, ref_vcf], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            proc = subprocess.Popen(["bcftools",
+                                     "query",
+                                     "-f",
+                                     r"%CHROM %ID %POS %REF %ALT\n",
+                                     "-r",
+                                     query_str,
+                                     ref_vcf],
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT)
             return proc.communicate()[0][:-1].split('\n')
         else:
             print("Mutation not yet offsetted. Please offset before querying.")
             return None
 
     def __repr__(self):
-        return "{}_{}{}{}".format(self.start, self.stop, self.mutation_type, self.change)
-    
+        return "{}_{}{}{}".format(
+            self.start,
+            self.stop,
+            self.mutation_type,
+            self.change)
+
     def __str__(self):
-        return "Start: {}, Stop: {}, Change: {}, Mutation type: {}".format(self.start, self.stop, self.change.upper(), self.mutation_type)
+        return "Start: {}, Stop: {}, Change: {}, Mutation type: {}".format(
+            self.start, self.stop, self.change.upper(), self.mutation_type)
 
-CONFIG_FILENAME = "config.yml"
-
-tile = application.Tile(args.index)
-varval = int(args.variant)
-
-# load reference vcf from config
-with open(CONFIG_FILENAME, 'r') as stream:
-    data_args = yaml.load(stream)
-    ref_vcf = data_args['ref_vcf']
-
-# retrieve tile information using getTileVariants script
-info_tile = getTileVariants.tile_iteration(tile, all_functionality = True, out="suppress")
-
-# break up output into common variant (.000) and specifically chosen variant
-tile_variant = info_tile.variants.split('\n')[varval]
-common_variant = info_tile.variants.split('\n')[0]
-
-if tile_variant == common_variant:
-    print("No difference between tiles. Exiting...")
-    sys.exit()
-
-# retrieve sequence, ignoring hash + id
-tile_seq = tile_variant.split(',')[2]
-common_seq = common_variant.split(',')[2]
-
-print("Common sequence: {}".format(common_seq))
-print("Variant sequence: {}".format(tile_seq))
-# delete spanning tile parts if necessary
-if len(tile_seq) - len(common_seq) >= 24:
-    tile_seq = tile_seq[:len(common_seq)]
-    print("Detected spanning tile. Deleting extra part...")
-elif len(common_seq) - len(tile_seq) >= 24:
-    common_seq = common_seq[:len(tile_seq)]
-    print("Detected spanning tile. Deleting extra part...")
-
-# run mutalyzer description extractor for alignment data
-allele = describe_dna(common_seq, tile_seq)
-changes = str(allele)
-
-# break up into list if needed
-changes = changes.replace('[', '')
-changes = changes.replace(']', '')
-changes = changes.split(';')
 
 # create a mutation object from the description extractor information
-def get_mutation(mutation_string):
+
+
+def get_mutation(mutation_string, info_tile):
     mutation_symbols = ['subst', 'delins', 'del', 'ins', 'dup', 'inv']
     for mutation in mutation_symbols:
         if mutation in mutation_string:
@@ -111,38 +95,128 @@ def get_mutation(mutation_string):
             else:
                 start = info[0]
                 stop = -1
-                
+
             if mutation == "del":
                 info[1] = common_seq[int(start):int(stop)]
-            return Mutation(start=start, stop=stop, change=info[1], mutation_type=mutation, chrom=info_tile.to_dict()['chrom'])
-            
-    else: # it would be a substitution (SNP)
-        # substitutions always follow the format #W>W. Break up into number then SNP content
+            return Mutation(
+                start=start,
+                stop=stop,
+                change=info[1],
+                mutation_type=mutation,
+                chrom=info_tile.to_dict()['chrom'])
+
+    else:  # it would be a substitution (SNP)
+        # substitutions always follow the format #W>W. Break up into number
+        # then SNP content
         num, mutation = mutation_string[:-3], mutation_string[-3:]
 
-        return Mutation(start=num, change=mutation, mutation_type="subst", chrom=info_tile.to_dict()['chrom']) 
+        return Mutation(
+            start=num,
+            change=mutation,
+            mutation_type="subst",
+            chrom=info_tile.to_dict()['chrom'])
 
-mutations = map(get_mutation, changes)
+def rsid_search(tile_index, variant, suppress_output=True):
+    results_dict = {}
 
-# offset all mutations
-map(lambda x: x.offset(info_tile.to_dict()['position_start']), mutations)
+    tile = application.Tile(tile_index)
+    varval = int(variant)
+    # retrieve tile information using getTileVariants script
+    info_tile = getTileVariants.tile_iteration(
+        tile, suppress_output=True, all_functionality=True)
 
-# store all the queries
-rsid_queries = map(lambda x: x.rsid_query(), mutations)
+    # break up output into common variant (.000) and specifically chosen variant
+    tile_variant = info_tile.variants.split('\n')[varval]
+    common_variant = info_tile.variants.split('\n')[0]
 
-rsid_queries = list(filter(lambda x: x != '', rsid_queries))
+    if tile_variant == common_variant:
+        print("No difference between tiles. Exiting...")
+        sys.exit()
 
-mutations_lst = zip(mutations, rsid_queries)
+    # retrieve sequence, ignoring hash + id
+    tile_seq = tile_variant.split(',')[2]
+    common_seq = common_variant.split(',')[2]
+    
+    results_dict['tile_sequence'] = tile_variant
+    results_dict['common_sequence'] = common_variant
+    results_dict['variant'] = variant
+    results_dict['index'] = tile_index
+    results_dict['mutations'] = {}
 
-# print results
-for mutation, rsid_lst in mutations_lst:
-    print('---')
-    print("Mutation: {}".format(mutation))
-    print("Representation: {}".format(repr(mutation)))
-    if len(rsid_lst) > 0 and rsid_lst[0] != '':
-        print("Possible SNP RSIDS:")
-        for rsid_query in rsid_lst:
-            chrom, rsid, location, ref, alt = rsid_query.split(" ") 
-            print("RSID: {}; Location: {}, REF: {}, ALT: {}".format(rsid, location, ref, alt))
-    else:
-        print("No possible SNPs found") 
+    if not suppress_output:
+        print("Common sequence: {}".format(common_seq))
+        print("Variant sequence: {}".format(tile_seq))
+    # delete spanning tile parts if necessary
+    if len(tile_seq) - len(common_seq) >= 24:
+        tile_seq = tile_seq[:len(common_seq)]
+        if not suppress_output: print("Detected spanning tile. Deleting extra part...")
+    elif len(common_seq) - len(tile_seq) >= 24:
+        common_seq = common_seq[:len(tile_seq)]
+        if not suppress_output: print("Detected spanning tile. Deleting extra part...")
+
+    # run mutalyzer description extractor for alignment data
+    allele = describe_dna(common_seq, tile_seq)
+    changes = str(allele)
+
+    # break up into list if needed
+    changes = changes.replace('[', '')
+    changes = changes.replace(']', '')
+    changes = changes.split(';')
+
+    mutations = map(lambda x: get_mutation(x, info_tile), changes)
+
+    # offset all mutations
+    map(lambda x: x.offset(info_tile.to_dict()['position_start']), mutations)
+
+    # store all the queries
+    rsid_queries = map(lambda x: x.rsid_query(), mutations)
+
+    rsid_queries = list(filter(lambda x: x != '', rsid_queries))
+
+    mutations_lst = zip(mutations, rsid_queries)
+
+    # print results
+    for mutation, rsid_lst in mutations_lst:
+        if not suppress_output:
+            print('---')
+            print("Mutation: {}".format(mutation))
+            print("Representation: {}".format(repr(mutation)))
+        
+        mutation_info = []
+        
+        if len(rsid_lst) > 0 and rsid_lst[0] != '':
+            if not suppress_output: print("Possible SNP RSIDS:")
+            for rsid_query in rsid_lst:
+                chrom, rsid, location, ref, alt = rsid_query.split(" ")
+                result_str = "RSID: {}; Location: {}, REF: {}, ALT: {}".format(
+                        rsid, location, ref, alt)
+                
+                if not suppress_output: print(result_str)
+                mutation_info.append((rsid, location, ref, alt))
+        else:
+            if not suppress_output: print("No possible SNPs found")
+                
+        results_dict['mutations'][repr(mutation)] = mutation_info
+            
+        
+    return results_dict
+
+if __name__ == "__main__":
+    # set up argument parsing
+    parser = argparse.ArgumentParser(
+        description="Retrieve a list of possible RSIDs a tile variant may contain")
+
+    parser.add_argument(
+        "-i",
+        "--index",
+        type=int,
+        help="Index of tile to search",
+        required=True)
+    parser.add_argument(
+        "-v",
+        "--variant",
+        type=int,
+        help="Which variant to search for RSIDs for (e.g. for varval 1, it will use the variant with id ending in .001)")
+    args = parser.parse_args()
+    rsid_search(args.index, args.variant, suppress_output=False)
+    
