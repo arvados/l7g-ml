c66e4f1e2abe4366294734725932d7ddcfe5f494
Sarah Wait Zaranek <swz@curii.com>
2021-02-03 21:11:20 +0000

    Updating files and figures Arvados-DCO-1.1-Signed-off-by: Sarah Wait Zaranek <swz@curii.com> no issue #

--- PublicDataExamples/NewFormat/loadingPGPBloodType_zygosity.py
+++ PublicDataExamples/NewFormat/loadingPGPBloodType_zygosity.py
@@ -29,17 +29,12 @@ dataBloodType = pgputils.yloadBlood(ydatasource,bloodtype)
 
 # Load Tile Data
 Xtrain = np.load(allfile)
-print(np.amax(Xtrain))
-print(np.amax(Xtrain,axis=0))
 
 # Low Quality Represented by -1 
 # Skipped Tiles Respresented by 0 
 Xtrain += Xtrain  
 idxN1  = Xtrain <= 0
 Xtrain[idxN1] = 0
-
-print(np.amax(Xtrain))
-print(np.amax(Xtrain,axis=0))
 
 #pathdata = np.load(infofile)
 # Pathdata not available
@@ -59,7 +54,11 @@ names = pgputils.pgpCleanNames(names)
 [Xtrain,y] = tileutils.syncTiles(dataBloodType,Xtrain,names)
 
 # Create Vector of Original Index of Tile Position
-idxOP = np.arange(Xtrain.shape[1])
+idxn = Xtrain.shape[1]/2
+idxrange = np.arange(idxn)
+idxOP = np.empty(Xtrain.shape[1])
+idxOP[0::2] = idxrange
+idxOP[1::2] = idxrange
 
 # Remove XYM Chromosomes
 #[Xtrain,pathdata,idxOP]  = tileutils.removeXYM(Xtrain,pathdata,idxOP)
@@ -74,9 +73,7 @@ tiledPCA = tileutils.pcaComponents(XtrainPCA,varvalsPCA,20)
 # Reshaping Matrix to Combine Phases  
 [m,n] = Xtrain.shape
 test = np.equal(Xtrain[:,0:n:2],Xtrain[:,1:n:2])
-print(test)
 count = np.count_nonzero(test)
-print(count)
 Xtrain = np.concatenate((Xtrain[:,0:n:2], Xtrain[:,1:n:2]),axis=0)
 pathdata = pathdata[0:n:2]
 idxOP = idxOP[0:n:2]
@@ -84,27 +81,10 @@ idxOP = idxOP[0:n:2]
 # Quality Cutoff 90% for Filter and Further ML
 [Xtrain, pathdata, idxOP] = tileutils.qualCutOff(Xtrain,pathdata,idxOP,0.90)
 [pathdataOH, idxOPOH, varvals]= tileutils.findTileVars(Xtrain,pathdata,idxOP)
-print(Xtrain.shape)
-print(varvals)
-idxvar = varvals > 2
-print(varvals[idxvar])
-
-quit()
 
 # Calculate OH Representation, Filtered using Pearson Chi2
-print("inputs to zygosity")
-print(Xtrain)
-print(pathdataOH)
-print(varvals)
-print(idxOPOH)
-print(y)
 # (tiledgenomes,tileposOH,idxOPOH,varvals,y,nparts,pcutoff):
 [Xtrain, pathdataOH, varvals, idxOPOH] = tileutils.chiZygosity(Xtrain,pathdataOH,idxOPOH,varvals,y,5,.02)
-
-print(Xtrain.shape)
-print(pathdataOH.shape)
-print(varvals.shape)
-print(idxOPOH.shape)
 
 # Combine Filtered OH Encoded Tiled Genomes and PCA Components
 tiledPCA = csr_matrix(tiledPCA)
