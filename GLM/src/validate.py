9821ce8c78af19d1edbc5f8ed82cb379b5a3be52
Jiayong Li <jli@curii.com>
2022-11-17 18:14:08 +0000

    Add comparison models to validate Arvados-DCO-1.1-Signed-off-by: Jiayong Li <jli@curii.com> refs #19528

--- GLM/src/validate.py
+++ GLM/src/validate.py
@@ -7,7 +7,7 @@ import sklearn as sk
 import sys
 import os
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import roc_auc_score, log_loss, confusion_matrix, ConfusionMatrixDisplay
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 
@@ -48,22 +48,14 @@ def make_matrix(row_column):
   matrix = sc.sparse.csr_matrix((data, (row, column)))
   return matrix
 
-def main():
-  onehotfile, onehotcolumnfile, samplesphenotypefile, countfile, fractionthreshold = sys.argv[1:]
-  row_column = np.load(onehotfile)
-  matrix = make_matrix(row_column)
-  onehot_columns = np.load(onehotcolumnfile)
-  fractionthreshold = float(fractionthreshold)
-  df = pd.read_table(samplesphenotypefile)
-  allphenotypes = df.columns.values.tolist()[4:]
-  tilevars, phenotypes = extract_tilevars(countfile, allphenotypes, fractionthreshold)
+def train_validate(phenotypes, column_indices, df, matrix, onehot_columns):
+  """Train and validate logistic regression model."""
   training_indices = df[df["status"]=="training"]["index"].to_numpy()
   training_ads = df[df["status"]=="training"]["AD"].to_numpy()
   training_phenotypes = df[df["status"]=="training"][phenotypes].to_numpy()
   validation_indices = df[df["status"]=="validation"]["index"].to_numpy()
   validation_ads = df[df["status"]=="validation"]["AD"].to_numpy()
   validation_phenotypes = df[df["status"]=="validation"][phenotypes].to_numpy()
-  column_indices = get_column_indices(onehot_columns, tilevars)
   # horizontally stack phenotype matrix with training/validation submatrix
   training_matrix = sc.sparse.hstack((training_phenotypes, matrix[training_indices][:, column_indices]))
   validation_matrix = sc.sparse.hstack((validation_phenotypes, matrix[validation_indices][:, column_indices]))
@@ -71,19 +63,76 @@ def main():
   clf = LogisticRegression(penalty='none', class_weight='balanced', max_iter=500).fit(training_matrix, training_ads)
   prediction = clf.predict(validation_matrix)
   coef = clf.coef_.flatten()
-  print("intercept = {}".format(clf.intercept_[0]))
-  score = sk.metrics.accuracy_score(validation_ads, prediction)
-  print("accuracy = {}".format(score))
+  accuracy = sk.metrics.accuracy_score(validation_ads, prediction)
+  roc_auc = roc_auc_score(validation_ads, clf.predict_proba(validation_matrix)[:,1])
+  log_likelihood = -log_loss(validation_ads, clf.predict_proba(validation_matrix))
   dict_output = {"feature": phenotypes +
                             ["{}-{}-{}".format(onehot_columns[0,i], onehot_columns[1,i], onehot_columns[2,i]) for i in column_indices],
                  "coef": coef}
   df_output = pd.DataFrame(dict_output)
   df_output = df_output.reindex(df_output["coef"].abs().sort_values(ascending=False).index)
-  df_output.to_csv("feature_coef.tsv", sep='\t', index=False)
   cm = confusion_matrix(validation_ads, prediction)
-  disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-  disp.plot()
-  plt.savefig('confusion_matrix.png')
+  display = ConfusionMatrixDisplay(confusion_matrix=cm)
+  return (accuracy, roc_auc, log_likelihood, df_output, display)
+
+def main():
+  onehotfile, onehotcolumnfile, samplesphenotypefile, countfile, fractionthreshold = sys.argv[1:]
+  row_column = np.load(onehotfile)
+  matrix = make_matrix(row_column)
+  onehot_columns = np.load(onehotcolumnfile)
+  df = pd.read_table(samplesphenotypefile)
+  allphenotypes = df.columns.values.tolist()[4:]
+  phenotypes_dict = {}
+  column_indices_dict = {}
+  accuracy_dict = {}
+  roc_auc_dict = {}
+  log_likelihood_dict = {}
+  df_output_dict = {}
+  display_dict = {}
+  # run the model with features selected by glmnetboot
+  fractionthreshold = float(fractionthreshold)
+  tilevars, phenotypes_dict["glmnetboot_features"] = extract_tilevars(countfile, allphenotypes, fractionthreshold)
+  column_indices_dict["glmnetboot_features"] = get_column_indices(onehot_columns, tilevars)
+  (accuracy_dict["glmnetboot_features"], roc_auc_dict["glmnetboot_features"], log_likelihood_dict["glmnetboot_features"],
+    df_output_dict["glmnetboot_features"], display_dict["glmnetboot_features"]) = train_validate(
+    phenotypes_dict["glmnetboot_features"], column_indices_dict["glmnetboot_features"], df, matrix, onehot_columns)
+  # locate apoe, which is the first feature selectd by glmnetboot
+  apoe_pos, apoe_num = df_output_dict["glmnetboot_features"]["feature"].to_list()[0].split("-")[:2]
+  apoe_pos, apoe_num = int(apoe_pos), int(apoe_num)
+  apoe_column_indices = [i for i in column_indices_dict["glmnetboot_features"]
+                         if onehot_columns[0,i] == apoe_pos and onehot_columns[1,i] == apoe_num]
+  apoe_column_indices = np.array(apoe_column_indices)
+  # set up comparison models
+  comparisonmodels= ["sex-age_normalized", "allphenotypes", "apoe",
+                     "sex-age_normalized-apoe", "allphenotypes-apoe"]
+  phenotypes_dict["sex-age_normalized"] = ["Sex", "Age_normalized"]
+  column_indices_dict["sex-age_normalized"] = np.array([])
+  phenotypes_dict["allphenotypes"] = allphenotypes
+  column_indices_dict["allphenotypes"] = np.array([])
+  phenotypes_dict["apoe"] = []
+  column_indices_dict["apoe"]= apoe_column_indices
+  phenotypes_dict["sex-age_normalized-apoe"] = ["Sex", "Age_normalized"]
+  column_indices_dict["sex-age_normalized-apoe"] = apoe_column_indices
+  phenotypes_dict["allphenotypes-apoe"] = allphenotypes
+  column_indices_dict["allphenotypes-apoe"] = apoe_column_indices
+  for model in comparisonmodels:
+    (accuracy_dict[model], roc_auc_dict[model], log_likelihood_dict[model],
+      df_output_dict[model], display_dict[model]) = train_validate(
+      phenotypes_dict[model], column_indices_dict[model], df, matrix, onehot_columns)
+  for model in ["glmnetboot_features"]+comparisonmodels:
+    print("model: {}".format(model))
+    print("accuracy: {}".format(accuracy_dict[model]))
+    print("roc_auc: {}".format(roc_auc_dict[model]))
+    if model in comparisonmodels:
+      degrees_of_freedom = np.abs(len(phenotypes_dict[model]) + len(column_indices_dict[model])
+        - len(phenotypes_dict["glmnetboot_features"]) - len(column_indices_dict["glmnetboot_features"]))
+      pvalue = 1-sc.stats.chi2.cdf(-2*(log_likelihood_dict[model] - log_likelihood_dict["glmnetboot_features"]), degrees_of_freedom)
+      print("pvalue comparing to glmnetboot_features: {}".format(pvalue))
+    print(df_output_dict[model].to_string(index=False))
+    print()
+    display_dict[model].plot()
+    plt.savefig("confusion_matrix_{}.png".format(model))
+  df_output_dict["glmnetboot_features"].to_csv("feature_coef.tsv", sep='\t', index=False)
 
 if __name__ == "__main__":
   main()
